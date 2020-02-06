06ab61d0ef1a2fb670b6c039c332bae2a9e3985a
Miguel <miguel@localhost.localdomain>
2020-02-06 16:37:01 -0400

    Compromiso4 p3

---
+++ Semestre8/Programacion3/Compromiso4/Tokens.h
@@ -0,0 +1,25 @@
+#ifndef TOKENS_H_INCLUDED
+#define TOKENS_H_INCLUDED
+
+/***
+ * Header file that contains all token code constants
+ * used by flex output source code
+ *
+ * @Author : Manuel Peralta.
+ *
+ * */
+
+#include <stdio.h>
+
+#define ID 1
+#define NUM 2
+#define PLUS 3
+#define MINUS 4
+#define MULT 5
+#define DIV 6
+#define ASGN 7
+#define SEMI 8
+#define LPAREN 9
+#define RPAREN 10
+
+#endif // TOKENS_H_INCLUDED
